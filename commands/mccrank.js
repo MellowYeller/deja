bfac1889768ac10204d2caf680375464b9397bfc
Mitchell Vivian <31528143+MellowYeller@users.noreply.github.com>
2020-08-17 19:19:50 -0700

    Add MCC rank but for real

---
+++ commands/mccrank.js
@@ -0,0 +1,58 @@
+const mcc = require('../mcc.js');
+const Discord = require('discord.js');
+
+module.exports = {
+	name: 'mccrank',
+	description: 'Test command for MCC',
+	aliases: ['mccranks', 'mccsr'],
+	usage: '[-VERSION] [GAMERTAG]',
+	args: true,
+	supportsProfiles: true,
+
+	async execute(message, args) {
+		let gamertag = '';
+		let version = 'windows';
+		if (args.length) {
+			if (args[0].startsWith('-')) {
+				version = args.shift().substring(1).toLowerCase();
+				if (version !== 'xbox-one' && version !== 'xbox' && version !== 'pc' && version !== 'windows') {
+					return message.reply('Invalid option. Use either \'-xbox\' or \'-pc\'. No option assumes Windows.');
+				}
+				switch (version) {
+				case 'xbox':
+					version = 'xbox-one';
+					break;
+				case 'pc':
+					version = 'windows';
+					break;
+				}
+			}
+		}
+		if (args.length) {
+			gamertag = args.join(' ');
+		}
+		else {
+			gamertag = message.client.profiles.get(message.author.id);
+		}
+		const json = await mcc.getRanks(version, gamertag);
+		const ranks = Object.keys(json);
+		gamertag = json[ranks[0]][0].Gamertag;
+		const data = [];
+		data.push('```');
+		data.push(' ______________________________ ');
+		data.push('|Playlist                 |Rank|');
+		data.push('|-------------------------|----|');
+		for (const rank of ranks) {
+			data.push(`|${rank.padEnd(25, ' ')}|${json[rank][0].SkillRank.toString().padStart(4, ' ')}|`);
+		}
+		data.push('|______________________________|');
+		data.push('```');
+		const embed = new Discord.MessageEmbed()
+			.setTitle(gamertag)
+			.setURL(`https://halowaypoint.com/en-us/games/halo-the-master-chief-collection/${version}/skill-ranks?gamertags=${gamertag.replace(' ', '%20')}`)
+			.addFields(
+				{ name: `MCC ${(version == 'windows') ? 'PC' : 'Xbox'} Ranks`, value: data },
+			);
+		message.channel.send(embed);
+	},
+};
